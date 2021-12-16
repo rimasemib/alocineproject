b95e5a27a42e89413114e8b427b9dc40dc648ffc
PierreM <7anUDQ2A3MVJkC7J@gmail.com>
2021-12-16 12:36:28 +0100

    url buttons

--- src/App.js
+++ src/App.js
@@ -6,30 +6,46 @@ import CartMovie from "./components/CartMovie";
 
 const App = () => {
   const [data, setData] = useState(null);
-  // piste : l'URL doit etre dans un state
-
   const url1 = "https://api-allocine.herokuapp.com/api/movies/popular";
   const url2 = "https://api-allocine.herokuapp.com/api/movies/upcoming";
   const url3 = "https://api-allocine.herokuapp.com/api/movies/top_rated";
+  // piste : l'URL doit etre dans un state
+  const [url, setUrl] = useState(url1);
 
   const fetchData = async () => {
-    const response = await axios.get(url2);
-    console.log(
-      "DATAS RECUPEREES APRES ATTENTE DU AWAIT:",
-      response.data.results
-    );
+    const response = await axios.get(url);
     setData(response.data.results);
   };
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [url]);
 
   const urlImgPrefix = "https://image.tmdb.org/t/p/w370_and_h556_bestv2";
 
   return (
     <div style={{ fontSize: "30px" }}>
-      BONJOUR
-      <span>PAGE</span>
+      HEADER ALLOCINE
+      <button
+        onClick={() => {
+          setUrl(url1);
+        }}
+      >
+        URL1 (POPULAR)
+      </button>
+      <button
+        onClick={() => {
+          setUrl(url2);
+        }}
+      >
+        URL2 (upcoming)
+      </button>
+      <button
+        onClick={() => {
+          setUrl(url3);
+        }}
+      >
+        URL3 (top rated)
+      </button>
       {data
         ? data.map((film, i) => {
             return (
